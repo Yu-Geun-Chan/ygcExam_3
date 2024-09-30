0b34143768a8f919ea7b2e6381f1a01a44ea89af
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-09-30 10:41:15 +0900

    tailwind와 mui 충돌 피하는법

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  /* tailwind와 mui의 충돌을 막기 위해 추가 */
+  corePlugins: {
+    preflight: false,
+  },
+  important: true,
+  prefix: 'tw-',
+  /* 여기까지 */
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
