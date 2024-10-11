8d39b6c7857230dd275b0ba2eb465b38cf99022b
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-10-11 14:33:36 +0900

    멀티라인 텍스트, Box

--- src/app/theme.js
+++ src/app/theme.js
@@ -2,10 +2,14 @@ import { createTheme } from '@mui/material';
 import { create } from '@mui/material/styles/createTransitions';
 
 const theme = createTheme({
+  typography: {
+    fontFamily: 'GmarketSansMedium',
+  },
   palette: {
     type: 'light',
     primary: {
       main: '#3f51b5',
+      dark: '#afafaf',
     },
     secondary: {
       main: '#f50057',
